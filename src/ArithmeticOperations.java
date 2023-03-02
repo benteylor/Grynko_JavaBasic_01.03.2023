394bc69d96f8aa7909875509e11db7663f40b04b
Andrey Grynko <benteylor@gmail.com>
2023-03-02 21:16:53 +0200

    Arithmetic operations

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -1,17 +1,12 @@
-public class HelloWorld {
+public class ArithmeticOperations {
     public static void main(String[] args) {
 
-   int a = 1;
+        int a = 1;
         int b = 2;
 
         System.out.println(b + a);
         int c = 8;
         int d = c++;
         System.out.println(c + d);
-
     }
-
-
 }
-
-

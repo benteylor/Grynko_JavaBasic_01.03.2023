71730f15798fee345ad3d465b53d69ef26da95c4
Andrey Grynko <benteylor@gmail.com>
2023-03-08 20:00:48 +0200

    MyApp

---
+++ src/MyApp.java
@@ -0,0 +1,8 @@
+public class MyApp {
+    public static void main(String[] args) {
+        double latitude = 46.3835;
+        double longitude = 30.7128;
+        System.out.println("Latitude: " + latitude);
+        System.out.println("Longitude: " + longitude);
+    }
+}
